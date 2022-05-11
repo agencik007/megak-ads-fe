cd3051c85d5dcb66911cda426f0d2f5c22a020ee
agencik007 <agencik007@gmail.com>
2022-05-11 23:18:12 +0200

    Added configuration to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import {TestInterface} from "types";
 import './App.css';
 
 function App() {
+    const foobar: TestInterface = {
+        x: 123,
+    };
+
   return (
     <div className="App">
       <header className="App-header">
