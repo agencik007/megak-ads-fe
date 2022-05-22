8b6912c1c623ac63e0567418cc34ec9f4cf9ff41
agencik007 <agencik007@gmail.com>
2022-05-22 11:43:37 +0200

    React router integrated and styles for button like links fixed.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {App} from "./App";
+import {BrowserRouter} from "react-router-dom";
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
